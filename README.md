9d54d52e5696dfffecc7f29549e1f926fc3d3b22
jaruche <jaruche@gmail.com>
2017-10-10 22:34:12 -0400

    Setting demo value to false and converting tabs to spaces

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 #### Instructions on how to setup Gazebo, ROS and other dependencies can be found [here](./README_Udacity.md).
 
-#### A comprehensive discussion around the mplementation can be found in this [Medium post](https://medium.com/@fernandojaruchenunes/udacity-robond-project-1-search-and-sample-return-2d8165a53a78).
+#### A comprehensive discussion around the mplementation can be found in this [Medium post](https://medium.com/@fernandojaruchenunes/udacity-robotics-nd-project-2-robotic-arm-pick-place-71af9c9ba519).
 
 ---
 
--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -40,13 +40,11 @@ def rot_z(q):
 
 ### Creates Homogeneous Transform Matrix
 def homogeneous_transform(q, d, a, alpha):
-    T = Matrix(
-[[ cos(q),           -sin(q),           0,             a          ],
-[ sin(q)*cos(alpha), cos(q)*cos(alpha), -sin(alpha), -sin(alpha)*d],
-[ sin(q)*sin(alpha), cos(q)*sin(alpha),  cos(alpha),  cos(alpha)*d],
-[                 0,                 0,           0,             1]]
-    )
-	return T
+    T = Matrix([[ cos(q),           -sin(q),           0,             a          ],
+                [ sin(q)*cos(alpha), cos(q)*cos(alpha), -sin(alpha), -sin(alpha)*d],
+                [ sin(q)*sin(alpha), cos(q)*sin(alpha),  cos(alpha),  cos(alpha)*d],
+                [                 0,                 0,           0,             1]])
+    return T
 
 def handle_calculate_IK(req):
     rospy.loginfo("Received %s eef-poses from the plan" % len(req.poses))
@@ -59,41 +57,41 @@ def handle_calculate_IK(req):
         # Create symbols
     print 'Creating symbols'
     # Joint angle (variable for revolute)
-	q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')
-	# Link offset (variable for prismatic)
-	d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
-	# Link length
-	a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
-	# Twist angles
-	alpha0,alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
+    q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')
+    # Link offset (variable for prismatic)
+    d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
+    # Link length
+    a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
+    # Twist angles
+    alpha0,alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
 
-	# Create Modified DH parameters
-	DH = {alpha0: 0,      a0: 0,      d1: 0.75,     q1: q1,
-     	  alpha1: -pi/2,  a1: 0.35,   d2: 0, 	    q2: q2-pi/2,
+    # Create Modified DH parameters
+    DH = {alpha0: 0,      a0: 0,      d1: 0.75,     q1: q1,
+          alpha1: -pi/2,  a1: 0.35,   d2: 0, 	    q2: q2-pi/2,
           alpha2: 0,      a2: 1.25,   d3: 0,	    q3: q3,
           alpha3: -pi/2,  a3: 0.0536, d4: 1.5014,   q4: q4,
           alpha4: pi/2,   a4: 0,      d5: 0,	    q5: q5,
           alpha5: -pi/2,  a5: 0,      d6: 0,        q6: q6,
           alpha6: 0,      a6: 0,      d7: 0.303,    q7: 0}
 
-	# Define Modified DH Transformation matrix
-	#
-	#
-	# Create individual transformation matrices
-	print 'Creating HTs'
-	T0_1 = homogeneous_transform(q1, d1, a0, alpha0).subs(DH)
-	T1_2 = homogeneous_transform(q2, d2, a1, alpha1).subs(DH)
-	T2_3 = homogeneous_transform(q3, d3, a2, alpha2).subs(DH)
-	T3_4 = homogeneous_transform(q4, d4, a3, alpha3).subs(DH)
-	T4_5 = homogeneous_transform(q5, d5, a4, alpha4).subs(DH)
-	T5_6 = homogeneous_transform(q6, d6, a5, alpha5).subs(DH)
-	T6_G = homogeneous_transform(q7, d7, a6, alpha6).subs(DH)
+    # Define Modified DH Transformation matrix
+    #
+    #
+    # Create individual transformation matrices
+    print 'Creating HTs'
+    T0_1 = homogeneous_transform(q1, d1, a0, alpha0).subs(DH)
+    T1_2 = homogeneous_transform(q2, d2, a1, alpha1).subs(DH)
+    T2_3 = homogeneous_transform(q3, d3, a2, alpha2).subs(DH)
+    T3_4 = homogeneous_transform(q4, d4, a3, alpha3).subs(DH)
+    T4_5 = homogeneous_transform(q5, d5, a4, alpha4).subs(DH)
+    T5_6 = homogeneous_transform(q6, d6, a5, alpha5).subs(DH)
+    T6_G = homogeneous_transform(q7, d7, a6, alpha6).subs(DH)
 
-	# Extract rotation matrices from the transformation matrices
-	# Generalized homogeneous transform
-	T0_G = T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6 * T6_G
+    # Extract rotation matrices from the transformation matrices
+    # Generalized homogeneous transform
+    T0_G = T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6 * T6_G
 
-	# Correcting difference in orientation between DH convention and URDF file
+    # Correcting difference in orientation between DH convention and URDF file
     r, p, y = symbols('r p y')
     R_x = rot_x(r)
     R_y = rot_y(p)
@@ -105,9 +103,9 @@ def handle_calculate_IK(req):
             # IK code starts here
             joint_trajectory_point = JointTrajectoryPoint()
 
-	    # Extract end-effector position and orientation from request
-	    # px,py,pz = end-effector position
-	    # roll, pitch, yaw = end-effector orientation
+        # Extract end-effector position and orientation from request
+        # px,py,pz = end-effector position
+        # roll, pitch, yaw = end-effector orientation
             px = req.poses[x].position.x
             py = req.poses[x].position.y
             pz = req.poses[x].position.z
@@ -117,58 +115,59 @@ def handle_calculate_IK(req):
                     req.poses[x].orientation.z, req.poses[x].orientation.w])
 
             ### Your IK code here
-	    # Compensate for rotation discrepancy between DH parameters and Gazebo
-	    print 'Processing pose: ' + str(x)
-	    R_corr = R_z.subs(y, pi) * R_y.subs(p, -pi/2)
-	    Rrpy = R_z * R_y * R_x * R_corr
-	    Rrpy = Rrpy.subs({'r': roll, 'p': pitch, 'y': yaw})
+        # Compensate for rotation discrepancy between DH parameters and Gazebo
+        print 'Processing pose: ' + str(x)
+        R_corr = R_z.subs(y, pi) * R_y.subs(p, -pi/2)
+        Rrpy = R_z * R_y * R_x * R_corr
+        Rrpy = Rrpy.subs({'r': roll, 'p': pitch, 'y': yaw})
 
-	    # Calculate joint angles using Geometric IK method
-	    # Calculate the wrist center position first
-	    print 'Calculating wrist center'
+        # Calculate joint angles using Geometric IK method
+        # Calculate the wrist center position first
+        print 'Calculating wrist center'
         nx = Rrpy[0,2]
         ny = Rrpy[1,2]
         nz = Rrpy[2,2]
 
-	    wx =  px - 0.303 * nx
-	    wy =  py - 0.303 * ny
-	    wz =  pz - 0.303 * nz
+        # d7 = 0.303
+        wx =  px - 0.303 * nx
+        wy =  py - 0.303 * ny
+        wz =  pz - 0.303 * nz
 
-	    print 'Calculating position angles'
-	    # Theta 1 from above is a simple atan2
-	    theta1 = atan2(wy, wx)
-	    # Calculate Radius from above
-	    r = sqrt(wx**2+wy**2) - 0.35
+        print 'Calculating position angles'
+        # Theta 1 from above is a simple atan2
+        theta1 = atan2(wy, wx)
+        # Calculate Radius from above
+        r = sqrt(wx**2+wy**2) - 0.35 # a1: 0.35
 
-	    # Calculating Theta 2 and 3 using cosine law. A, B and C are sides of the triangle
-	    A = 1.501
-	    B = sqrt(r**2+(wz-0.75)**2)
-	    C = 1.25
+        # Calculating Theta 2 and 3 using cosine law. A, B and C are sides of the triangle
+        A = 1.501
+        B = sqrt(r**2+(wz-0.75)**2) # d1: 0.75
+        C = 1.25
 
-	    # a corresponds to angle apha
-	    a = acos((B**2 + C**2 - A**2) / (2*B*C))
-	    theta2 = pi/2 - a - atan2(wz-0.75, r)
+        # a corresponds to angle apha
+        a = acos((B**2 + C**2 - A**2) / (2*B*C))
+        theta2 = pi/2 - a - atan2(wz-0.75, r)  # d1: 0.75
 
-	    # b corresponds to angle beta
-	    b = acos((A**2 + C**2 - B**2) / (2*A*C))
-	    theta3 = pi/2 - (b + 0.036)
+        # b corresponds to angle beta
+        b = acos((A**2 + C**2 - B**2) / (2*A*C))
+        theta3 = pi/2 - (b + 0.036)
 
-	    print 'Calculating orientation angles'
-	    # Calculating Euler angles from orientation
-	    R0_3 = T0_1[0:3,0:3] * T1_2[0:3,0:3] * T2_3[0:3,0:3]
-	    R0_3 = R0_3.evalf(subs={'q1':theta1, 'q2':theta2, 'q3':theta3})
-	    #R3_6 = R0_3.inv("LU")*Rrpy # using transpose instead:
-	    R3_6 = R0_3.T * Rrpy
+        print 'Calculating orientation angles'
+        # Calculating Euler angles from orientation
+        R0_3 = T0_1[0:3,0:3] * T1_2[0:3,0:3] * T2_3[0:3,0:3]
+        R0_3 = R0_3.evalf(subs={'q1':theta1, 'q2':theta2, 'q3':theta3})
+        #R3_6 = R0_3.inv("LU")*Rrpy # using transpose instead:
+        R3_6 = R0_3.T * Rrpy
 
-	    theta4 = atan2(R3_6[2,2], -R3_6[0,2])
-	    theta5 = atan2(sqrt(R3_6[0,2]**2 + R3_6[2,2]**2), R3_6[1,2])
-	    theta6 = atan2(-R3_6[1,1], R3_6[1,0])
+        theta4 = atan2(R3_6[2,2], -R3_6[0,2])
+        theta5 = atan2(sqrt(R3_6[0,2]**2 + R3_6[2,2]**2), R3_6[1,2])
+        theta6 = atan2(-R3_6[1,1], R3_6[1,0])
 
-	    print 'Appending calculated values'
+        print 'Appending calculated values'
             # Populate response for the IK request
             # In the next line replace theta1,theta2...,theta6 by your joint angle variables
-	    joint_trajectory_point.positions = [theta1, theta2, theta3, theta4, theta5, theta6]
-	    joint_trajectory_list.append(joint_trajectory_point)
+        joint_trajectory_point.positions = [theta1, theta2, theta3, theta4, theta5, theta6]
+        joint_trajectory_list.append(joint_trajectory_point)
 
         rospy.loginfo("length of Joint Trajectory List: %s" % len(joint_trajectory_list))
         return CalculateIKResponse(joint_trajectory_list)
